ac4fb47567cafc00affa7314b9b5c5ad74a83761
Dustin Feurich <s0540042@htw-berlin.de>
2017-01-24 00:37:58 +0100

    TestMain hinzugefügt

---
+++ raspberry/asip/TestMain.py
@@ -0,0 +1,13 @@
+from PeerSemanticTag import  *
+from PredicateSemanticTag import *
+
+
+def main():
+    address_max = Address("mail://s0123456@htw-berlin.de")
+    address_maxima = Address("mail://s0123457@htw-berlin.de")
+    peer_tag_max = PeerSemanticTag("Max Mustermann", "https://studi.f4.htw-berlin.de/~s0123456/", address_max)
+    peer_tag_maxima = PeerSemanticTag("Maxima Mustermann", "https://studi.f4.htw-berlin.de/~s0123457/", address_maxima)
+    predicate_tag = PredicateSemanticTag("Marriage", "https://en.wikipedia.org/wiki/Marriage", peer_tag_max.si, peer_tag_maxima.si)
+    print("Predicate Tag:   " + predicate_tag.__str__())
+
+if  __name__ =='__main__':main()
