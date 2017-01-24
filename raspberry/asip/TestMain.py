1405e4b38fcfa44827573996cc269330b94a8ccb
Dustin Feurich <s0540042@htw-berlin.de>
2017-01-24 01:09:57 +0100

    Anpassungen

--- raspberry/asip/TestMain.py
+++ raspberry/asip/TestMain.py
@@ -9,5 +9,7 @@ def main():
     peer_tag_maxima = PeerSemanticTag("Maxima Mustermann", "https://studi.f4.htw-berlin.de/~s0123457/", address_maxima)
     predicate_tag = PredicateSemanticTag("Marriage", "https://en.wikipedia.org/wiki/Marriage", peer_tag_max.si, peer_tag_maxima.si)
     print("Predicate Tag:   " + predicate_tag.__str__())
+    print("Max:"   + address_max.__str__())
+    print("Maxima:"   + address_maxima.__str__())
 
 if  __name__ =='__main__':main()
